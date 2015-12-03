148686fcd7762f7e2ae0fd3040541c381a17d801
Casey Kolderup <casey.kolderup@voxmedia.com>
2015-12-03 15:39:49 -0500

    let's perl it up

--- 2/part2.rb
+++ 2/part2.rb
@@ -1,6 +1,4 @@
 puts (IO.readlines('input.txt').inject(0) do |m, p|
-  (l,w,h) = p.split('x').map(&:to_i)
-  shortest = [l,w,h]
-  shortest.delete_at([l,w,h].each_with_index.max[1])
-  m + shortest.inject(0) { |m, k| m + k * 2 } + l * w * h
+  d = p.split('x').map(&:to_i).sort!
+  m + d.take(2).inject(0) { |mm, k| mm + k*2 } + d.inject(1) { |mm,k| mm*k }
 end)
